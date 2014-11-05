7d3a0c964ff6b320959b6bdef968e071ebcf32df
John Doe <john@sample.com>
2014-11-05 22:24:34 +0530

    changes

--- Salesforce_New/src/com/sfdc/pom/AccountDetailPage.java
+++ Salesforce_New/src/com/sfdc/pom/AccountDetailPage.java
@@ -14,9 +14,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
@@ -24,22 +21,18 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AccountDetailPage extends BasePage {
 	private WebDriver driver;
-	
-	
-	static XSSFRow rownum1;
-	static XSSFRow row_account;
+
+	static Row rownum1;
+	static Row row_account;
 	static String Field_Name;
 	static String Account_Name;
 	static int record_found = 0;
-	
-	
-	
+
 	int exit_loop = 0;
 	int exec_end = 0;
 
@@ -61,36 +54,59 @@ public class AccountDetailPage extends BasePage {
 		this.driver = driver;
 	}
 
-	public void AccountDetailVerification(String AccountName) throws InterruptedException, IOException, InvalidFormatException {
-		Actions actions = new Actions(driver);
-		FileInputStream fis=new FileInputStream(new File(xlPath));
-		FileOutputStream file1 = new FileOutputStream(new File(xlPath));
-		org.apache.poi.ss.usermodel.Workbook wb = WorkbookFactory.create(fis);
-		org.apache.poi.ss.usermodel.Sheet s1 = wb.getSheet("Fields Repository");
-		
-		do{
-		driver.switchTo().defaultContent();
+	public void AccountDetailVerification(String AccountName, String xlPath,
+			int j) throws InterruptedException, IOException,
+			InvalidFormatException {
 
-	
+		FileInputStream fis = new FileInputStream(xlPath);
+
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet s1 = wb.getSheet("Fields Repository");
+
+		exit_loop = 0;
+		exec_end = 0;
 		int i = 0;
-		int j = 2;
-		
-		
+
+		row_account = s1.getRow(i);
+		/*
+		 * if(counter>0) {
+		 * 
+		 * driver.findElement(By.xpath("//a[contains(text(), 'My Accounts')]")).
+		 * click();
+		 * 
+		 * driver.switchTo().frame("itarget");
+		 * 
+		 * }
+		 */
+
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+		j++;
+
+		// try {
+		// Account_Name
+		// =row_account.getCell(j).getStringCellValue();}catch(NullPointerException
+		// fsf){exec_end=5;}
+
+		// if(exec_end!=5)
+		// {
+
 		driver.findElement(By.xpath(".//*[@id='bodyCell']/div[3]"));
 
-		((JavascriptExecutor) driver).executeScript("arguments[0].style.backgroundColor='yellow';",AccountDetail);
+		((JavascriptExecutor) driver).executeScript(
+				"arguments[0].style.backgroundColor='yellow';", AccountDetail);
 
 		AccountRecordType.click();
 
 		String Account_record_type = AccountRecordType.getText();
 
-		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File scrFile = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
 
-		String filepath = "C:\\Users\\sharath\\Desktop\\"+ AccountName + ".png";
+		String filepath = "C:\\Users\\udanka\\Desktop\\" + AccountName+ ".png";
 
 		FileUtils.copyFile(scrFile, new File(filepath));
 
-		
 		Iterator<Row> iterator1 = s1.iterator();
 
 		do {
@@ -110,7 +126,7 @@ public class AccountDetailPage extends BasePage {
 				if (Account_record_type.equals(record_type)) {
 					i = rowcount;
 					record_found = 3;
-					
+
 				}
 			}
 		} while (iterator1.hasNext());
@@ -119,10 +135,10 @@ public class AccountDetailPage extends BasePage {
 
 		do {
 
-			rownum1 = (XSSFRow) s1.getRow(i);
+			rownum1 = (Row) s1.getRow(i);
 			try {
 				Field_Name = rownum1.getCell(1).getStringCellValue();
-				System.out.println("1st Field" + Field_Name);
+				System.out.println("Field number "+ i+" is " + Field_Name);
 			} catch (NullPointerException ds) {
 
 				System.out.println("Exitingloop");
@@ -131,14 +147,11 @@ public class AccountDetailPage extends BasePage {
 
 			if (exit_loop < 1) {
 
-				driver.manage().timeouts()
-						.implicitlyWait(2, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 				try {
 					String ab = driver.findElement(
-							By.xpath("//td[(text()='" + Field_Name
-									+ "')]")).getText();
-					System.out.println("The field " + ab
-							+ "is present in the page");
+							By.xpath("//td[(text()='" + Field_Name + "')]")).getText();
+					System.out.println("The field " + ab+ "is present in the page");
 					rownum1.createCell(j).setCellValue("PASS");
 
 				} catch (NoSuchElementException fd)
@@ -148,16 +161,13 @@ public class AccountDetailPage extends BasePage {
 					try {
 
 						String ab = driver.findElement(
-								By.xpath("//span[(text()='"
-										+ Field_Name + "')]"))
-								.getText();
-						System.out.println("The field " + ab
-								+ "is present in the page");
+								By.xpath("//span[(text()='" + Field_Name+ "')]")).getText();
+						System.out.println("The field " + ab+ "is present in the page");
 						rownum1.createCell(j).setCellValue("PASS");
 					} catch (NoSuchElementException sd)
 
 					{
-						System.out.println("The field " + Field_Name+ "is not present in the page");
+						System.out.println("The field " + Field_Name+ " is not present in the page");
 						rownum1.createCell(j).setCellValue("FAIL");
 					}
 				}
@@ -165,12 +175,15 @@ public class AccountDetailPage extends BasePage {
 			i++;
 		} while (exit_loop != 5);
 
-	}
- while (exec_end != 5);
-System.out.println("Execution ended no more fields");
+		// }
+		// }while (exec_end != 5);
 
-wb.write(file1);
-file1.close();
+		System.out.println("Execution ended no more fields");
+
+		FileOutputStream file1 = new FileOutputStream(xlPath);
+
+		wb.write(file1);
+		file1.close();
 
 	}
 }
--- Salesforce_New/src/com/sfdc/scripts/SFDC_AccountVerification.java
+++ Salesforce_New/src/com/sfdc/scripts/SFDC_AccountVerification.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.server.browserlaunchers.Sleeper;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import com.lib.ExcelLib;
 import com.sfdc.pom.BasePage;
 import com.sfdc.pom.AccountDetailPage;
@@ -24,62 +23,76 @@ public class SFDC_AccountVerification extends SuperTestNG {
 			NullPointerException, InvalidFormatException, IOException {
 		LoginPage loginPage = new LoginPage(driver);
 		BasePage basePage = new BasePage(driver);
-		String xlPath ="D:/Selenium/Workspace/Udanka/test data_Final.xlsx";
-		
-		int rowCount = ExcelLib.getRowCount(xlPath, "Login and Expected Accounts");
-			    
-		
-		for (int i = 1; i <= rowCount; i++) {
-			
-			String Uname = ExcelLib.getCellValue(xlPath, "Login and Expected Accounts", i, 0);
-			String password = ExcelLib.getCellValue(xlPath, "Login and Expected Accounts", i,	1);
-			loginPage.login(Uname, password);
-			
-			int j=2;
-			int colCount = ExcelLib.getColCount(xlPath, "Login and Expected Accounts", j);
-			
-			for (j = 2; j <= colCount; j++){
-				//int colCount = ExcelLib.getColCount(xlPath, "Login and Expected Accounts", j);
-				String accountName = ExcelLib.getCellValue(xlPath, "Login and Expected Accounts", i, j);
-				
-			
-			basePage.goToMyAccounts();
+		String xlPath = "C:/Users/udanka/workspace/test data_Final.xlsx";
 
-			GotoAccountDetailPage gotoPage = new GotoAccountDetailPage(driver);
-			gotoPage.gotoAccountDetail();
-			
-			//String xp = "//a[contains(text(),'" + accountName + "')]";  
-			Actions actions = new Actions(driver);
-			
-			WebDriverWait wait = new WebDriverWait(driver,15);
-			
-			
-			int temp=0;
-			do {
-				temp=0;
-				try{
-					
-			WebElement account = wait.until(ExpectedConditions.elementToBeClickable(((By.xpath("//a[text()='"+accountName+"']")))));
-			
-			actions.moveToElement(account);
-			actions.click();
-			actions.perform();
-			
-			}catch(ElementNotFoundException e) 
-				{
-				temp =5;
-				gotoPage.clickNext();
-				} 
+		int rowCount = ExcelLib.getRowCount(xlPath,
+				"Login and Expected Accounts");
+
+		for (int i = 1; i <= rowCount; i++) {
+
+			String Uname = ExcelLib.getCellValue(xlPath,
+					"Login and Expected Accounts", i, 0);
+			String password = ExcelLib.getCellValue(xlPath,
+					"Login and Expected Accounts", i, 1);
+			loginPage.login(Uname, password);
+
+			int j = 2;
+			int dummy = 0;
+			int colCount = 0;
+			try {
+				colCount = ExcelLib.getColCount(xlPath,
+						"Login and Expected Accounts", i);
+			} catch (NullPointerException d) {
+				dummy++;
 			}
-			while (temp ==5);
-			
-			Sleeper.sleepTightInSeconds(10);
-			
-			AccountDetailPage temp1= new AccountDetailPage(driver);
-			temp1.AccountDetailVerification(accountName);
-			
+
+			if (dummy < 1) {
+				for (j = 2; j <= colCount - 2; j++) {
+					// int colCount = ExcelLib.getColCount(xlPath,
+					// "Login and Expected Accounts", j);
+					String accountName = ExcelLib.getCellValue(xlPath,
+							"Login and Expected Accounts", i, j);
+
+					basePage.goToMyAccounts();
+
+					GotoAccountDetailPage gotoPage = new GotoAccountDetailPage(
+							driver);
+					gotoPage.gotoAccountDetail();
+
+					// String xp = "//a[contains(text(),'" + accountName +
+					// "')]";
+					Actions actions = new Actions(driver);
+
+					WebDriverWait wait = new WebDriverWait(driver, 15);
+
+					int temp = 0;
+					do {
+						temp = 0;
+						try {
+
+							WebElement account = wait.until(ExpectedConditions
+									.elementToBeClickable(((By
+											.xpath("//a[text()='" + accountName
+													+ "']")))));
+
+							actions.moveToElement(account);
+							actions.click();
+							actions.perform();
+
+							AccountDetailPage temp1 = new AccountDetailPage(driver);
+							temp1.AccountDetailVerification(accountName,xlPath, j);
+
+						} catch (TimeoutException e) {
+							temp = 5;
+							gotoPage.clickNext();
+						}
+					} while (temp == 5);
+
+				}
+
+				basePage.logout();
 			}
 		}
-			basePage.logout();
-	
-}}
+
+	}
+}
